97873aa96add0ed0cf8462aacdd9abbde66f3fa3
yuhwanwoo <6514687@naver.com>
2025-02-10 07:36:55 +0900

    시큐리티 init

---
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -0,0 +1,12 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
