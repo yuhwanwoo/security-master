659d6690590a5c82a0aea2bd932440136913678f
yuhwanwoo <6514687@naver.com>
2025-03-04 07:22:28 +0900

    설정된 컨트롤러 추가

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -9,4 +9,14 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
 }
