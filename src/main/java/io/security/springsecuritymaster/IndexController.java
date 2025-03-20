bfd0fbc859c55191c2000104a60bb4ea015514e0
yuhwanwoo <6514687@naver.com>
2025-03-20 13:05:01 +0900

    anonymous 예시

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,5 +1,9 @@
 package io.security.springsecuritymaster;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +22,24 @@ public class IndexController {
     @GetMapping("/loginPage")
     public String loginPage() {
         return "loginPage";
+    }
+
+    @GetMapping("/anonymous")
+    public String anonymous() {
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication) {
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "anonymous";
+        } else {
+            return "not anonymous";
+        }
+    }
+
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext securityContext) {
+        return securityContext.getAuthentication().getName();
     }
 }
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -25,16 +25,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/anonymous").hasRole("GUEST")
+                        .requestMatchers("/anonymousContext", "/authentication").permitAll()
+                        .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .rememberMe(rememberMe -> rememberMe
-                        .alwaysRemember(true)
-                        .tokenValiditySeconds(3600)
-                        .userDetailsService(userDetailsService())
-                        .rememberMeParameter("remember")
-                        .rememberMeCookieName("remember")
-                        .key("security")
-                )
+                .anonymous(anonymous -> anonymous
+                        .principal("guest")
+                                .authorities("ROLE_GUEST")
+                        )
+
                 ;
         return http.build();
     }
